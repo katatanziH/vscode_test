94db8543733551bfc90111efdb40b64398d7901d
kats2020 <harrison.katantazi@gmail.com>
2020-08-21 12:29:49 +0200

    firstCommit

---
+++ add_naturalnumbers.py
@@ -0,0 +1,18 @@
+# Sum of natural numbers up to num
+import math
+import os
+import random
+
+name = input("Your Name! ")
+print("Hello !", name, "Solve this problem")
+num = 16
+
+ if num < 0:
+ print("Enter a positive number")
+# else:
+# sum = 0
+# use while loop to iterate until zero
+# while num > 0:
+# sum += num
+# num -= 1
+# print("The sum is", sum)
